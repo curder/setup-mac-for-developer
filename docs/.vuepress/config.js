9f80ead219fc0bf02d81031fafb8ad073ac25cfd
curder <q.curder@gmail.com>
2021-03-30 01:36:11 +0800

    change default config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,9 +3,19 @@ module.exports = {
   title: "MacOSX开发软件配置",
   description: "",
   themeConfig: {
-    editLinks: false,
+    repo: 'curder/setup-mac-for-developer',
+    editLinks: true,
+    editLinkText: "编辑页面",
+    // docsRepo: 'https://github.com/curder/setup-mac-for-developer',
+    docsBranch: 'master',
+    docsDir: 'docs',
+    editLinkPattern: ':repo/-/edit/:branch/:path',
     displayAllHeaders: false,
     sidebarDepth: 2,
+    lastUpdated: true,
+    lastUpdatedText: '最后更新时间',
+    contributors: true,
+    contributorsText: '贡献者列表',
     nav: [{ text: "README", link: "/" }],
     sidebar: {
       "/": require("./sidebar")
