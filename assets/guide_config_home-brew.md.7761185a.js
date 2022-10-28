261b6e24fa0858bcc0583460cc2081d85ba5639a
curder <curder@users.noreply.github.com>
2022-10-28 03:17:49 +0000

    deploy: 98abe386bad3063ed26acefb141a4ea8e5dc8938

---
+++ assets/guide_config_home-brew.md.7761185a.js
@@ -0,0 +1,28 @@
+import{_ as s,c as n,o as a,a as e}from"./app.c30a3164.js";const h=JSON.parse('{"title":"Homebrew \u5B89\u88C5\u4F9D\u8D56","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898","slug":"\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898","link":"#\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898","children":[]}],"relativePath":"guide/config/home-brew.md","lastUpdated":1666926661000}'),l={name:"guide/config/home-brew.md"},p=e(`<h1 id="homebrew-\u5B89\u88C5\u4F9D\u8D56" tabindex="-1">Homebrew \u5B89\u88C5\u4F9D\u8D56 <a class="header-anchor" href="#homebrew-\u5B89\u88C5\u4F9D\u8D56" aria-hidden="true">#</a></h1><div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">brew install libzip \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libpq \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> mozjpeg \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> pngquant \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> wget \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> autoconf \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> openssl \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libjpeg \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libpng \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> lzlib \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> curl \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> imap-uw \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> readline \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> postgresql \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libxml2 \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> mcrypt \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> gettext \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libxslt \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> libiconv \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> icu4c \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> expat \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> bison \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> webp \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> freetype \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> tree \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> pkg-config \\</span></span>
+<span class="line"><span style="color:#A6ACCD;"> telnet --verbose</span></span>
+<span class="line"></span></code></pre></div><h2 id="\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898" tabindex="-1">\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898 <a class="header-anchor" href="#\u89E3\u51B3\u56FD\u5185\u901F\u5EA6\u6162\u7684\u95EE\u9898" aria-hidden="true">#</a></h2><p>\u5982\u679C\u5728\u5B89\u88C5\u7684\u8FC7\u7A0B\u4E2D\u6BD4\u8F83\u6162\u7684\u8BDD\u53EF\u4EE5\u901A\u8FC7\u4E0B\u9762\u7684\u94FE\u63A5\u89E3\u51B3\u66F4\u65B0\u4E0B\u8F7D homebrew \u6162\u7684\u95EE\u9898</p><ul><li><a href="https://www.raydbg.com/2019/Homebrew-Update-Slow/" target="_blank" rel="noreferrer">\u89E3\u51B3 Homebrew \u4E0B\u8F7D\u66F4\u65B0\u6781\u6162\u7684\u95EE\u9898</a></li><li><a href="https://xu3352.github.io/mac/2018/09/06/mac-homebrew-update-slowly" target="_blank" rel="noreferrer">Mac brew update \u975E\u5E38\u6162... \u66F4\u6362 brew \u955C\u50CF\u6E90\u89E3\u51B3</a></li></ul>`,5),o=[p];function c(t,r,i,C,A,d){return a(),n("div",null,o)}const y=s(l,[["render",c]]);export{h as __pageData,y as default};
